0111038bf80542494d66f7dff2a7dc5f4adc95f8
kernel.dang <kerneldang@outlook.com>
2024-07-03 18:13:05 +0700

    hide

--- src/_layout/layout.tsx
+++ src/_layout/layout.tsx
@@ -55,7 +55,7 @@ export const Layout = ({ header, content, bottom }: LayoutProps) => {
       <button
         type="button"
         onClick={printToPdf}
-        class="fixed bottom-2 right-2 z-10 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+        class="fixed hidden bottom-2 right-2 z-10 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
       >
         Print CV as PDF
       </button>
