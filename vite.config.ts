c61e5c743c1d15c0e44847f6e38b1b7eef93d6b3
kernel.dang <kerneldang@outlook.com>
2024-07-03 10:24:37 +0700

    update confige

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import preact from '@preact/preset-vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [preact()],
+  /**
+   * refer https://vitejs.dev/guide/static-deploy.html to deploying to Github Pages
+   */
+  base: '/my-cv/'
 })
