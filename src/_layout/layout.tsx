ae4af10568fd8c13d5c1d19e2df3443b34e8131c
kernel.dang <kerneldang@outlook.com>
2024-07-03 17:58:47 +0700

    xxx

--- src/_layout/layout.tsx
+++ src/_layout/layout.tsx
@@ -26,8 +26,8 @@ export const Layout = ({ header, content, bottom }: LayoutProps) => {
 
         pdfDoc.addImage({
           imageData: imageData,
-          x: 100,
-          y: 100,
+          x: 0,
+          y: 0,
           width: pdfPageWidth,
           height: pdfPageHeight,
         });
@@ -54,6 +54,7 @@ export const Layout = ({ header, content, bottom }: LayoutProps) => {
       </div>
       <button
         type="button"
+        onClick={printToPdf}
         class="fixed bottom-2 right-2 z-10 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
       >
         Print CV as PDF
